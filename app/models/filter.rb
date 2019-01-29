2ae298891797d3979880af554471ae27d8bb9c98
Adam Ruzicka <aruzicka@redhat.com>
2019-01-29 16:23:08 +0100

    Message matching implementation and tests

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -12,4 +12,33 @@ class Filter < ApplicationRecord
 
   has_many :event_type_filters
   has_many :event_types, :through => :event_type_filters, :dependent => :destroy
+
+  class << self
+    def matching_message(message)
+      sql = <<~SQL
+        SELECT filters.* FROM filters
+          LEFT OUTER JOIN app_filters
+            ON app_filters.filter_id = filters.id
+          LEFT OUTER JOIN event_type_filters
+            ON event_type_filters.filter_id = filters.id
+          LEFT OUTER JOIN severity_filters
+            ON severity_filters.filter_id = filters.id
+          LEFT OUTER JOIN apps
+            ON apps.id = app_filters.app_id
+          LEFT OUTER JOIN event_types
+            ON event_types.id = event_type_filters.event_type_id AND event_types.app_id = apps.id
+          WHERE
+            (app_filters.app_id IS NULL
+              OR
+              (apps.name = ?
+                AND
+                (event_type_filters.event_type_id IS NULL
+                  OR event_types.name = ?)))
+             AND (severity_filters.filter_id IS NOT NULL
+                   AND (severity_filters.severity IN (?)
+                         OR severity_filters.severity IS NULL))
+      SQL
+      Filter.find_by_sql sanitize_sql_array([sql, message[:application], message[:type], message[:severity]])
+    end
+  end
 end
--- test/models/filter_test.rb
+++ test/models/filter_test.rb
@@ -11,9 +11,68 @@ class FilterTest < ActiveSupport::TestCase
 
   should have_many(:event_type_filters)
   should have_many(:event_types).through(:event_type_filters)
+
+  before do
+    Builder::App.build! do |a|
+      a.name 'not_useful'
+      event_types.each { |type| a.event_type type }
+    end
+  end
+
+  let(:msg) do
+    { :application => app.name, :type => app.event_types.first.name, :severity => 'critical' }
+  end
+  let(:event_types) { %w(something something-else yet-something-else) }
+  let(:app_name) { 'filter-test-app-1' }
+  let(:app) do
+    Builder::App.build! do |a|
+      a.name app_name
+      event_types.each { |type| a.event_type type }
+    end
+  end
+
+  it 'allows matching by application name, event type and severity' do
+    Builder::Filter.build! do |b|
+      b.application(app.name, app.event_types.first.name)
+      b.severity 'low'
+    end
+
+    filter = Builder::Filter.build! do |b|
+      b.application app.name, app.event_types.first.name
+      b.severities 'critical', 'high'
+    end
+
+    Filter.matching_message(msg).must_equal [filter]
+  end
+
+  it 'allows matching by application name, event type and severity wildcard' do
+    filter = Builder::Filter.build! do |b|
+      b.application app.name, app.event_types.first.name
+      b.severity.any!
+    end
+
+    Filter.matching_message(msg).must_equal [filter]
+  end
+
+  it 'allows matching by application name, event type wildcard and severity wildcard' do
+    filter = Builder::Filter.build! do |b|
+      b.application(app.name).event_type.any!
+      b.severities 'critical', 'high'
+    end
+
+    Filter.matching_message(msg).must_equal [filter]
+  end
+
+  it 'allows matching by application name wildcard, event type wildcard and severity' do
+    filter = Builder::Filter.build! do |b|
+      b.application.any!
+      b.severities 'critical', 'high'
+    end
+
+    Filter.matching_message(msg).must_equal [filter]
+  end
 end
 
 class SeverityFilterTest < ActiveSupport::TestCase
-  should validate_presence_of(:severity)
   should belong_to(:filter)
 end
