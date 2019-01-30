7a47e8e904d7be7cee51b26ecbdf8020eb7eb265
Adam Ruzicka <a.ruzicka@outlook.com>
2019-01-30 10:09:39 +0100

    rubocop -a --only Style/FrozenStringLiteralComment

--- app/services/builder.rb
+++ app/services/builder.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Builder
   class Filter
     def initialize
--- app/services/builder/app.rb
+++ app/services/builder/app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Builder
   class App
     def event_types
