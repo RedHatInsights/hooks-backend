f8cc9c81304af8ac80f96129301a0ae95a49e182
Shimon Shtein <sshtein@redhat.com>
2019-02-04 21:21:50 +0200

    Add topic pruning consumer

---
+++ app/consumers/null_consumer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'notifications'
+
+class NullConsumer < Racecar::Consumer
+  subscribes_to Notifications::INCOMING_TOPIC
+
+  def process(kafka_message)
+    Rails.logger.info("Discarded message: #{kafka_message.value}")
+  end
+end
