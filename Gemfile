d2879be86eecdc54850109f357e3edda283c0d64
Adam Ruzicka <aruzicka@redhat.com>
2019-02-11 09:18:56 +0100

    Drop all references to Capybara

--- Gemfile
+++ Gemfile
@@ -40,7 +40,6 @@ gem 'will_paginate'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-  gem 'capybara-webkit'
   gem 'codecov', :require => false
   gem 'factory_bot'
   gem 'minitest', '~> 5.1'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,9 +24,6 @@ require 'rspec/rails'
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/test/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
