5fe0aab9cc5a48d8bce523333d185ec3669a6735
Adam Ruzicka <aruzicka@redhat.com>
2019-02-12 15:13:01 +0100

    Make rubocop happy

---
+++ config/initializers/rsqag_ui.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Rswag::Ui.configure do |c|
+  c.swagger_endpoint '/r/insights/platform/notifications/api-docs/v1/swagger.json', 'API V1 Docs'
+end
--- config/initializers/rswag-ui.rb
+++
@@ -1,3 +0,0 @@
-Rswag::Ui.configure do |c|
-  c.swagger_endpoint "/r/insights/platform/notifications/api-docs/v1/swagger.json", "API V1 Docs"
-end
