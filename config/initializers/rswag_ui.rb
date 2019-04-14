c7a3e3c0723f703a883c6a501528c3830835aa46
Adam Růžička <adamruzicka@users.noreply.github.com>
2019-04-14 11:22:12 +0200

    Convert swagger doc to OpenAPI v3 (#47)
    * Enable swagger->openapi v3 conversion
    
    * Convert swagger api doc to openapi v3 during s2i build
    
    * Trim down package.json
    
    * Run conversion in travis to check it works
    
    * Add node_modules to gitignore
    
    * Add OpenAPI v3 to swagger UI
    
    * Make rubocop happy

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rswag::Ui.configure do |c|
-  c.swagger_endpoint "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/api-docs/v1/swagger.json", 'API V1 Docs'
+  c.swagger_endpoint(
+    "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/api-docs/v1/openapi.json",
+    'API V1 OpenAPI v3 Docs'
+  )
+  c.swagger_endpoint(
+    "#{ENV['PATH_PREFIX']}/#{ENV['APP_NAME']}/api-docs/v1/swagger.json",
+    'API V1 Swagger Docs'
+  )
 end
